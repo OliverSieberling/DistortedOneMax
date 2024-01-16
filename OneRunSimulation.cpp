e389d1a4bb37e857e161d0a023c71ef53ebe6e60
OliverSieberling <75339002+OliverSieberling@users.noreply.github.com>
2024-01-16 21:55:08 +0100

    Update OneRunSimulation.cpp

--- OneRunSimulation.cpp
+++ OneRunSimulation.cpp
@@ -1,23 +1,21 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
+// returns a copy of the parent, where each bit is flipped with probability 1/n
 vector<char> generateChild(vector<char>& par) {
-    vector<char> child(par.size());
+    vector<char> child(par);
     random_device rd;
     mt19937 gen(rd());
-    bernoulli_distribution dist(1.0/(int)par.size());
-
-    for (int64_t i = 0; i < par.size(); i++) {
-        if (dist(gen)) {
-            child[i] = 1-par[i];
-        } else {
-            child[i] = par[i];
+    uniform_int_distribution<> dis(0, par.size());
+    for (int64_t i = 0; i < child.size(); i++) {
+        if (dis(gen) == 0) {
+            child[i] = 1-child[i];
         }
     }
     return child;
 }
 
+// returns if v1 and v2 are equal
 bool isEqual(vector<char> &v1, vector<char> &v2) {
     for (int i = 0; i < v1.size(); i++) {
         if(v1[i] != v2[i]) return false;
@@ -25,78 +23,102 @@ bool isEqual(vector<char> &v1, vector<char> &v2) {
     return true;
 }
 
+
+// returns fitness of a point; when called for the first time on this point samples distortion and computes fitness
 long double getFitness(vector<char> &point, map<vector<char>, long double> &fitness, long double p) {
     if (fitness.count(point)==0) {
         random_device rd;
         mt19937 gen(rd());
-        bernoulli_distribution dis(p); // distort with probability
-        if (dis(gen)) { // distorted point
-            random_device rd2;
-            mt19937 gen2(rd2());
-            exponential_distribution<double> dist2(0.4);
-           // cauchy_distribution<double> dist2(0, 0.3);
-            //uniform_real_distribution<> dist2(0, 5.0);
-            double distortion = abs(dist2(gen2));
+        bernoulli_distribution dis(p); // distort with probability p
+        if (dis(gen)) {
+            exponential_distribution<double> dis2(0.4);
+            //uniform_real_distribution<> dis2(0, 5.0);
+            double distortion = dis2(gen);
             fitness[point] = count(point.begin(), point.end(), 1) + distortion;
-        } else { // not distorted point
+        } else {
             fitness[point] = count(point.begin(), point.end(), 1);
         }
     }
     return fitness[point];
 }
 
+// simulates optimizing distorted OneMax on (1+lambda)-EA (isElitary = true) or (1-lambda)-EA (isElitary = false). Outputs whenever a different point is accepted.
 int64_t simulate(vector<char> currPoint, map<vector<char>,long double> &fitness, long double p, int64_t cutoff, int64_t lambda, long double k, bool isElitary) {
     int64_t genCounter = 1;
-    while (getFitness(currPoint, fitness, p) < (int64_t)currPoint.size()-k && genCounter < cutoff) {
+    long double currentPointFitness = getFitness(currPoint, fitness, p);
+    while (currentPointFitness < (int64_t)currPoint.size()-k && genCounter < cutoff) {
         genCounter++;
-        vector<char> fittestChild = generateChild(currPoint);
+        long double fittestChildFitness = -1;
+        vector<vector<char>> fittestChildren;
 
-        for (int64_t i = 0; i < lambda-1; i++) {
+        for (int64_t i = 0; i < lambda; i++) {
             vector<char> child = generateChild(currPoint);
+            long double childFitness = getFitness(child, fitness, p);
 
-            if (getFitness(child, fitness, p) > getFitness(fittestChild, fitness, p)) {
-                fittestChild = child;
+            if (childFitness > fittestChildFitness) {
+                fittestChildren.clear();
+                fittestChildFitness = childFitness;
+                fittestChildren.push_back(child);
+            } else if (childFitness == fittestChildFitness) {
+                fittestChildren.push_back(child);
             }
         }
+
+        // tie brake uniformly at random between offspring of equal fitness
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_int_distribution<> dis(0, fittestChildren.size()-1);
+        vector<char> fittestChild = fittestChildren[dis(gen)];
+
+        long double fittestChildDistortion = fittestChildFitness - count(fittestChild.begin(), fittestChild.end(), 1);
+
         if (isElitary) {
-            if (getFitness(fittestChild, fitness, p) > getFitness(currPoint, fitness, p)) {
+            if (fittestChildFitness >= currentPointFitness) {
+                if (!isEqual(currPoint, fittestChild)) { // do not print if fittestChild is copy of parent
+                    cout  << setw(13) << left << genCounter << setw(13) << left <<  fittestChildDistortion << setw(13) << left << fittestChildFitness << "\n";
+                }
                 currPoint = fittestChild;
-                cout << genCounter << " " << count(currPoint.begin(), currPoint.end(), 1) << " " << (getFitness(currPoint, fitness, p)-count(currPoint.begin(), currPoint.end(), 1)) << "\n";
+                currentPointFitness = fittestChildFitness;
             }
         } else {
-            if (!isEqual(currPoint, fittestChild)) {
-                cout << genCounter << " " << count(fittestChild.begin(), fittestChild.end(), 1) << " " << (getFitness(fittestChild, fitness, p)-count(fittestChild.begin(), fittestChild.end(), 1)) << "\n";
+            if (!isEqual(currPoint, fittestChild)) { // do not print if fittestChild is copy of parent
+                cout  << setw(13) << left << genCounter << setw(13) << left <<  fittestChildDistortion << setw(13) << left << fittestChildFitness << "\n";
             }
             currPoint = fittestChild;
-
-
+            currentPointFitness = fittestChildFitness;
         }
     }
-    //cout << genCounter << " " << getFitness(currPoint, fitness, p) << " " << count(currPoint.begin(), currPoint.end(), 1) << endl;
     return genCounter;
 
 }
 
 int main()
 {
-
-    /*string filename = "simulate_uniformTo5_iter=" + to_string(numIter) + "_from=" + to_string(from) + "_to=" + to_string(to) + "_steps=" +to_string(stepSize)+ "_cutoff=" +to_string(cutoff) + ".txt";
+    string filename = "simulate_exp04_iter=" + to_string(numIter) + "_from=" + to_string(from) + "_to=" + to_string(to) + "_steps=" +to_string(stepSize)+ "_cutoff=" +to_string(cutoff) + ".txt";
     ofstream outFile(filename);
     streambuf* coutBuffer = cout.rdbuf();
-    cout.rdbuf(outFile.rdbuf());*/
+    cout.rdbuf(outFile.rdbuf());
 
     int64_t n= 100;
-    long double p = 0.3/(sqrt(n)); // distortion probability
+    long double p = 0.3/(sqrt(n));
     long double k = pow(n, 0.15);
-    int64_t lambda = round( (exp(1)/(exp(1)-1)) * log(n));
+    int64_t lambda = round((exp(1)/(exp(1)-1)) * log(n));
     int64_t cutoff = 1'000'000;
     cout << n-k << endl;
     map<vector<char>,long double> fitness;
-    vector<char> initSearchPoint(n, 0);
-    simulate(initSearchPoint, fitness, p, cutoff, lambda, k, false);
+
+    // choose initial search point uniformly at random
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(0, 1);
+    vector<char> initSearchPoint(n);
+    for (int i = 0; i < n; i++) {
+        initSearchPoint[i] = dis(gen);
+    }
+
+    simulate(initSearchPoint, fitness, p, cutoff, lambda, k, true);
 
     return 0;
 }
-
 
 
